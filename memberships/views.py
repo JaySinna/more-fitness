7a24d2c78fb50a124eb9a072f9968e0aea6eaacc
Jay Sinnadurai <jay.sinnadurai@gmail.com>
2025-04-04 15:53:04 +0100

    Create view for subscribing to memberships

--- memberships/views.py
+++ memberships/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from .models import Membership
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect
+from .models import Membership, Subscription
+from profiles.models import UserProfile
 
 def membership_list(request):
     """ A view to show all membership details """
@@ -11,3 +14,18 @@ def membership_list(request):
     }
 
     return render(request, 'memberships/membership_list.html', context)
+
+
+@login_required
+def subscribe_to_membership(request, membership_id):
+    """ Subscribe the logged-in user to the selected membership. """
+
+    membership = get_object_or_404(Membership, id=membership_id)
+    user_profile = get_object_or_404(UserProfile, user=request.user)
+
+    subscription, created = Subscription.objects.get_or_create(user_profile=user_profile)
+    subscription.membership = membership
+    subscription.is_active = True
+    subscription.save()
+
+    return redirect('membership_list')
