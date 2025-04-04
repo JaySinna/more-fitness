dd872ac8c955a4102da0383433a721b30667dc08
Jay Sinnadurai <jay.sinnadurai@gmail.com>
2025-04-04 15:39:49 +0100

    Added view, urls and template for displaying list of memberships

--- memberships/views.py
+++ memberships/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Membership
 
-# Create your views here.
+def membership_list(request):
+    """ A view to show all membership details """
+
+    memberships = Membership.objects.all()
+
+    context = {
+        'memberships': memberships,
+    }
+
+    return render(request, 'memberships/membership_list.html', context)
