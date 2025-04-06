907c7e09890f636a9e91da2abb744a310a7673d7
Jay Sinnadurai <jay.sinnadurai@gmail.com>
2025-04-06 18:21:33 +0100

    Create views, urls and templates for exercise and nutrition plan pages, and access denied page

--- memberships/urls.py
+++ memberships/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.membership_list, name='membership_list'),
     path('subscribe/<int:membership_id>/', views.subscribe_to_membership, name='subscribe_to_membership'),
     path('success/', views.subscription_success, name='subscription_success'),
+    path('exercise-plans/', views.exercise_plans, name='exercise_plans'),
+    path('nutrition-plans/', views.nutrition_plans, name='nutrition_plans'),
+    path('access-denied/', views.access_denied, name='access_denied'),
 ]
--- memberships/views.py
+++ memberships/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect
-from .models import Membership, Subscription
+from .models import Membership, Subscription, ExercisePlan, NutritionPlan
 from profiles.models import UserProfile
 
 def membership_list(request):
@@ -43,3 +43,43 @@ def subscription_success(request):
     """A view to confirm successful subscription."""
 
     return render(request, 'memberships/subscription_success.html')
+
+
+@login_required
+def exercise_plans(request):
+    """ Display exercise plans to subscribed users only. """
+
+    user_profile = request.user.userprofile
+    subscription = getattr(user_profile, 'subscription', None)
+
+    if subscription and subscription.is_active:
+        plans = ExercisePlan.objects.filter(membership=subscription.membership)
+        context = {
+            'plans': plans,
+        }
+        return render(request, 'memberships/exercise_plans.html', context)
+    else:
+        return render(request, 'memberships/access_denied.html')
+    
+
+@login_required
+def nutrition_plans(request):
+    """ Display nutrition plans to subscribed users only. """
+
+    user_profile = request.user.userprofile
+    subscription = getattr(user_profile, 'subscription', None)
+
+    if subscription and subscription.is_active:
+        plans = NutritionPlan.objects.filter(membership=subscription.membership)
+        context = {
+            'plans': plans,
+        }
+        return render(request, 'memberships/nutrition_plans.html', context)
+    else:
+        return render(request, 'memberships/access_denied.html')
+    
+
+def access_denied(request):
+    """ Deny access to fitness plans to users without an active subscription. """
+
+    return render(request, 'memberships/access_denied.html')
